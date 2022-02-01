e16a81bb0f6040bbc7afa5b4fdcb2827b507a339
Achhoubi Plus <78739445+abdoachhoubi@users.noreply.github.com>
2022-02-01 01:23:26 +0100

    Update script.js

--- Day 7 - To-Do List/script.js
+++ Day 7 - To-Do List/script.js
@@ -3,17 +3,21 @@ let target = document.querySelector(".tasks");
 let content = document.querySelector("#task__value");
 let err = document.querySelector(".error");
 let keys = [];
+let map = new Map();
+let key = "";
+let ver = "";
 
 addtask.addEventListener("click", function(){
     if (content.value !== ""){
-        keys.push(content.value);
+        key = "item" + Date.now();
+        keys.push(key);
         window.localStorage.setItem("keys", keys);
-        window.localStorage.setItem(content.value, content.value);
-        newElement(content.value);
+        map.set(key, content.value);
+        localStorage.myMap = JSON.stringify(Array.from(map.entries()));
+        newElement(content.value, key);
         content.value = "";
     } else {
         err.classList.remove("none");
-        console.log(content.value);
     }
 });
 
@@ -22,11 +26,12 @@ content.addEventListener("focus", function(){
 });
 
 // Adding new tasks to the list
-function newElement(ele){
+function newElement(ele, key){
     
     let container = document.createElement("div");
     let button = document.createElement("button");
     button.setAttribute("class", "delete");
+    button.setAttribute("key", key);
 
     container.setAttribute("id", "task");
     container.setAttribute("class", "task");
@@ -44,50 +49,22 @@ function newElement(ele){
     target.append(container);
 
     button.addEventListener("click", function(){
-        let ver = keys.indexOf(button.previousElementSibling.textContent);
-        let del__it = button.previousElementSibling.textContent;
-        keys.splice(ver , 1);
+        localStorage.clear();
+        ver = button.getAttribute("key");
+        keys.splice(keys.indexOf(ver) , 1);
         window.localStorage.setItem("keys", keys);
-        window.localStorage.removeItem(del__it);
+        map.delete(ver);
+        localStorage.myMap = JSON.stringify(Array.from(map.entries()));
         button.parentElement.remove();
     });
 };
 
-function addElement(ele){
-    let container = document.createElement("div");
-    let button = document.createElement("button");
-    button.setAttribute("class", "delete");
-
-    container.setAttribute("id", "task");
-    container.setAttribute("class", "task");
-
-    let text = document.createElement("p");
-    text.setAttribute("class", "paragraph");
-
-    let task = document.createTextNode(ele);
-    let btn = document.createTextNode("delete");
-
-    button.append(btn);
-    text.append(task);
-    container.append(text);
-    container.append(button);
-    target.append(container);
-
-    button.addEventListener("click", function(){
-        let ver = keys.indexOf(button.previousElementSibling.textContent);
-        let del__it = button.previousElementSibling.textContent;
-        keys.splice(ver , 1);
-        window.localStorage.setItem("keys", keys);
-        window.localStorage.removeItem(del__it);
-        button.parentElement.remove();
-    });
-}
-
 window.addEventListener("load", function() {
     if(localStorage.getItem("keys") !== ""){
         keys = localStorage.getItem("keys").split(",");
+        map = new Map(JSON.parse(localStorage.myMap));
         for (let i = 0; i < keys.length; i++){
-            newElement(localStorage.getItem(keys[i]));
+            newElement(map.get(keys[i]), keys[i]);
         }
     }
     err.classList.add("none");
